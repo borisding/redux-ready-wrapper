a7db41943d9e739d507c010b9b26a3690f757db9
borisding <boris.ding82@outlook.com>
2017-08-13 15:49:46 +0800

    added `wrap` function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
+const INVALID_FUNCTION = 'Invalid callback function!';
+const isFunction = fn => typeof fn === 'function';
+
 /**
- * Ready wrapper by passing a `callback` function.
- * `options` object is optional, can be passed at action basis.
- * ready action will be dispatched with provided `options` before invoking the `callback` function.
- * This allows us to add reducer for managing state at ready action level with provided `options`.
+ * Ready function that accepts a `callback` and optional `options` as params
+ * An ready action will be dispatched with provided options
+ * before proceeding to the next targeted action
  *
  * @param  {Function} callback
  * @param  {Object}   [options={}]
  * @return {Object}
  */
 export function ready(callback, options = {}) {
-  const type = 'READY_ACTION';
-  const readyActionCreator = () => ({ type, options });
-
   return ({ dispatch, getState }) => {
     const p = new Promise((resolve, reject) => {
-      if (typeof callback !== 'function') {
-        reject('Invalid callback function!');
+      if (!isFunction(callback)) {
+        reject(INVALID_FUNCTION);
       } else {
-        resolve(dispatch(readyActionCreator()));
+        resolve(dispatch({ type: 'READY_ACTION', options }));
       }
     });
 
@@ -26,17 +25,33 @@ export function ready(callback, options = {}) {
 }
 
 /**
- * ready action wrapper middleware.
+ * Wrap function that accepts a `callback` as param
+ * NO ready action will be dispatched
+ *
+ * @param  {Function} callback
+ * @return {Object}
+ */
+export function wrap(callback) {
+  return ({ dispatch, getState }) =>
+    new Promise((resolve, reject) => {
+      if (!isFunction(callback)) {
+        reject(INVALID_FUNCTION);
+      } else {
+        resolve(callback(dispatch, getState));
+      }
+    });
+}
+
+/**
+ * Redux ready wrapper middleware.
  * Need to be imported as part of application's middlewares for usage.
  *
  * @return {Function|Object}
  */
-export default () => (
-  store => next => (action) => {
-    if (typeof action === 'function') {
-      return action(store);
-    }
-
-    return next(action);
+export default () => store => next => (action) => {
+  if (typeof action === 'function') {
+    return action(store);
   }
-);
+
+  return next(action);
+};
